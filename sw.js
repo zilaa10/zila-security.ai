e9b399b2f0cddb113d6e135c700d3f68a0a67519
zilaa10 <ilhamzil02@gmail.com>
2026-01-20 13:42:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,37 @@
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+const CACHE_NAME = "zila-security-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/security.html",
+  "/ai.html",
+  "/css/style.css",
+  "/js/three-bg.js",
+  "/js/lang.js",
+  "/js/chatbot.js",
+  "/js/script.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
