2dc744fe9acb1b774253086b2433aedb4cefb9c4
zilaa10 <ilhamzil02@gmail.com>
2026-01-20 13:26:13 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// Simple security animation effect
 document.addEventListener("DOMContentLoaded", () => {
-  const blocks = document.querySelectorAll(".block");
-
-  let index = 0;
-  setInterval(() => {
-    blocks.forEach(b => b.style.boxShadow = "none");
-    blocks[index].style.boxShadow = "0 0 20px #3b82f6";
-    index = (index + 1) % blocks.length;
-  }, 1200);
+  console.log("ZILA Security Website Loaded");
 });
